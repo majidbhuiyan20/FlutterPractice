a3f8a27029940a48575a202272d2b1f5ad2cb14d
majidbhuiyan20 <65237569+majidbhuiyan20@users.noreply.github.com>
2025-05-27 01:05:51 +0600

    Successfully Signup

--- lib/auth/screen/signup_screen.dart
+++ lib/auth/screen/signup_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_practice/auth/service/auth_service.dart';
 import 'package:flutter_practice/widgets/my_button.dart';
 
+import '../../widgets/snack_bar.dart';
 import 'login_screen.dart';
 
 class SignupScreen extends StatefulWidget {
@@ -13,6 +15,36 @@ class SignupScreen extends StatefulWidget {
 class _SignupScreenState extends State<SignupScreen> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+  String? _emailError;
+  String? _passwordError;
+  final AuthService _authService = AuthService();
+  bool isLoadin = false;
+  bool isPasswordHidden = true;
+  void _signUp() async{
+    setState(() {
+      _emailError = null;
+      _passwordError = null;
+    });
+    final String email = emailController.text.trim();
+    final String password = passwordController.text.trim();
+    // if(!email.contains(".com")){
+    //   showSnackBar(context, "Invalid Email. It must contain .com", color: Colors.red);
+    // }
+    final result = await _authService.signUp(email, password);
+    if(result==null){
+      //Success Case
+      showSnackBar(context, "Sign Up Successful and Turned to Login", color: Colors.green);
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginScreen()));
+    }else{
+      //Error Case
+      showSnackBar(context, result);
+    }
+
+
+  }
+
+
+
   @override
   void dispose() {
     emailController.dispose();
@@ -43,6 +75,7 @@ class _SignupScreenState extends State<SignupScreen> {
               TextField(
                 controller: emailController,
                 decoration: InputDecoration(
+                  errorText: _emailError,
                   labelText: "Enter Email",
                   border: OutlineInputBorder(),
                   prefixIcon: Icon(Icons.email),
@@ -59,20 +92,28 @@ class _SignupScreenState extends State<SignupScreen> {
               TextField(
                 controller: passwordController,
                 decoration: InputDecoration(
+                  errorText: _passwordError,
                   labelText: "Enter Password",
-                  suffixIcon: IconButton(onPressed: (){}, icon: Icon(Icons.visibility)),
+                  suffixIcon: IconButton(onPressed: (){
+                    setState(() {
+                      isPasswordHidden = !isPasswordHidden;
+                    });
+                  }, icon: Icon(isPasswordHidden ? Icons.visibility_off:Icons.visibility)),
                   border: OutlineInputBorder(
                     borderSide: BorderSide(color: Colors.grey),
                     borderRadius: BorderRadius.all(Radius.circular(10)),
                   ),
                   prefixIcon: Icon(Icons.lock),
                 ),
+                obscureText: isPasswordHidden,
               ),
               SizedBox(height: 20,),
+
+              isLoadin ? Center(child: CircularProgressIndicator()) :
               SizedBox(
                 width: double.infinity,
                 child: MyButton(
-                    onTap: () {}, buttonText: "Sign Up", color: Colors.blueAccent),
+                    onTap: () {_signUp();}, buttonText: "Sign Up", color: Colors.blueAccent),
               ),
               SizedBox(height: 20,),
 
